c6ac4d6676e747a01b22458bc773015e6d38a784
ChangMo Yang <dcyang@skku.edu>
2025-08-14 13:18:11 +0900

    constants.py

---
+++ vmc_mlsw/constants.py
@@ -0,0 +1,8 @@
+# Jastrow and other global constants
+
+# Cutoff distance for electron-electron Jastrow
+JASTROW_EE_L_CUT = 7.5
+# Power parameter in Jastrow factor cutoff polynomial
+JASTROW_EE_M_POWER = 3
+# Electron-electron cusp condition value (like-spin)
+EE_CUSP_VALUE = 0.25
