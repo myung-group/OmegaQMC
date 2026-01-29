6d2accad15cc2fa47a05dc8c18dfae966faa7709
David 창모 Yang <dcyang@users.noreply.github.com>
2026-01-29 18:16:25 +0900

    vmcopt_gto_fast.py -> vmcopt_gto.py

--- tests/test_LiH_opt.py
+++ tests/test_LiH_opt.py
@@ -1,7 +1,7 @@
 import jax
 import jax.numpy as jnp
 from vmc_mlsw import generate_molecular_orbitals
-from vmc_mlsw.vmcopt_gto_fast import get_vmcopt_func
+from vmc_mlsw.vmcopt_gto import get_vmcopt_func
 
 rng_key = jax.random.key(888)
 
